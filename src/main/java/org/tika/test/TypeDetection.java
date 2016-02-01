2a8713d63e78b1b2991ff9c567839e03c507fa9a
Mohammad Yazdani <m.yazdani2010@gmail.com>
2016-01-31 20:13:22 -0800

    more example
    Read Image and get metadata of file

--- src/main/java/org/tika/test/TypeDetection.java
+++ src/main/java/org/tika/test/TypeDetection.java
@@ -1,9 +1,17 @@
 package org.tika.test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.logging.Handler;
 
 import org.apache.tika.Tika;
+import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
 
 
 public class TypeDetection {
@@ -21,8 +29,46 @@ public class TypeDetection {
       
       /***** content extraction *****/
       String filecontent = tika.parseToString(file);
-      System.out.println("Extracted Content: " + filecontent);
+//      System.out.println("Extracted Content: " + filecontent);
+
+
+      /******* AutoDetectParser *******/
       
+      //parse method parameters
+      Parser parser = new AutoDetectParser();
+      BodyContentHandler handler = new BodyContentHandler();
+      Metadata metadata = new Metadata();
+      FileInputStream inputstream = new FileInputStream(file);
+//      InputStream stream = TikaInputStream.get(file); //instead of FileinputStreamer you can use InputStream of Tika library
+      ParseContext context = new ParseContext();
+      
+      //parsing the file
+      parser.parse(inputstream, handler, metadata, context);
+      System.out.println("File content : " + handler.toString());
+ 
+      
+      /******* Metadata *******/
+      System.out.println("MetaData********");
+      Parser picParser = new AutoDetectParser();
+      BodyContentHandler picHandler = new BodyContentHandler();
+      Metadata picMetadata = new Metadata();
+      File picFile = new File("C:/Users/New/Desktop/AlphaDev.png");
+
+      //Parser method parameters
+      FileInputStream picInputstream = new FileInputStream(picFile);
+      ParseContext picContext = new ParseContext();
+      
+      parser.parse(picInputstream, picHandler, picMetadata, picContext);
+      System.out.println(picHandler.toString());
+
+      //getting the list of all meta data elements 
+      String[] picMetadataNames = picMetadata.names();
+
+      for(String name : picMetadataNames) {		        
+         System.out.println(name + ": " + picMetadata.get(name));
+      }
+      
+//      System.out.println(picMetadata.get("IHDR"));
       
       System.out.println("********** END: TypeDetection ********");
    }
